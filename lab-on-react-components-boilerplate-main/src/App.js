01c1d7dd82b302cf7be388e31f2b9ca438796628
Akash Singh <113436653+a5h101@users.noreply.github.com>
2022-12-23 16:21:56 +0530

    Update App.js

--- lab-on-react-components-boilerplate-main/src/App.js
+++ lab-on-react-components-boilerplate-main/src/App.js
@@ -7,8 +7,7 @@ function App() {
   return (
     <div>
       <GallaryHeader/>
-      <GallaryBody/>
-      {/* adding footer component */}
+      <GallaryBody/
       <GallaryFooter/> 
     </div>
   )
